b27ff7c764a84e6f57f2113e6afd79ac237297e7
SarankiruthikSanthanakrishnan <skiruthik2510@gmail.com>
2025-08-29 10:50:35 +0530

    Initial commit

---
+++ src/main/java/org/saran/insurancetracker/InsuranceTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.saran.insurancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/saran/insurancetracker/InsuranceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saran.insurancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
